946922b4128a1da319a1520a1651ed731eb22b78
Will Midgette <wamidgette@gmail.com>
2023-01-01 15:11:09 -0500

    prep for heroku deployment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
 ReactDOM.render(
-  <BrowserRouter basename='explore-a-city'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
